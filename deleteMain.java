7b03afde2ec9d43799f6dcd789a7a8bd61ff61e3
DanielAndersonn <82004712+DanielAndersonn@users.noreply.github.com>
2021-04-08 11:56:38 -0400

    Add files via upload

---
+++ deleteMain.java
@@ -0,0 +1,15 @@
+
+
+public class deleteMain(
+    public static void main(string[] args) (
+        list< String> numbers = new Arraylist <>()
+    number.add(5);
+    
+    Sytem.out.printin(numbers)
+
+
+
+
+    )
+
+)
